36a7b69d950573b7c4e84480d433930ca604afae
DouglasH9 <douglasherman@douglasrmansmbp.gateway.2wire.net>
2014-09-03 03:57:53 -0500

    first commit

---
+++ fizzbuzz.js
@@ -0,0 +1,52 @@
+
+$(document).ready(function (){
+
+//gets number entered in form
+var num = document.getElementById('num');
+var startingNum = num.value;
+
+
+//vars that will append div with fizzBuzz number to DOM
+var newLi = document.createElement('li');
+newLi.setAttribute('id', 'numLi');
+
+
+
+	
+//prints fizzBuzz loop
+var loop = function () {
+	for (startingNum; startingNum <= 100; startingNum++) {	
+
+		
+
+		if ((startingNum%3==0)&&(startingNum%5==0)){
+			
+			newLi.innerHTML = 'fizzBuzz!!!';
+		}
+
+		else if (startingNum%5==0){
+			
+			newLi.innerHTML = 'buzz';
+		}
+
+		else if (startingNum%3==0){
+			
+			newLi.innerHTML= 'fizz';	
+		}
+
+		else {
+			
+			newLi.innerHTML = startingNum;	
+		}
+
+		document.getElementById('printOut').appendChild(newLi.cloneNode(true));
+		console.log (startingNum);
+	}
+}
+
+
+
+//adds onClick event listener to button
+document.getElementById('fizzEnter').addEventListener('click', loop);
+
+});
